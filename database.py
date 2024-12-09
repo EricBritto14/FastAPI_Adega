04ca55a1efd43664b1172b748acdca943601e255
Barreto Eric (CaP/ETS2 GS/PSC3-AM2) <abe4ca@bosch.com>
2024-12-09 16:26:04 -0300

    :hammer: fix: Fixing the database security creating .env, and updating te requirements.

--- database.py
+++ database.py
@@ -1,11 +1,13 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
 
 #Creates database engine
-URL_DATABASE = "postgresql://admin:SmtCM92QkRUo3ms9JjbsQbUu34x6MvBu@dpg-ctbjetrqf0us73cn2i30-a.oregon-postgres.render.com/adega_santadose"
+URL_DATABASE = os.getenv("URL_DATABASE")
 # "postgresql://postgres:e40024041@localhost:5432/database_adega"
-
+if not URL_DATABASE:
+    raise ValueError("A URL do banco de dados não está sendo possível acessar.")
 
 engine = create_engine(URL_DATABASE) #Criando a engine, o banco de dados em si, e colocando o tipo dele, no caso a conexão com o postgress
 
